47bc8ddfe83d30a39c45e44891b72e428b0d4ad3
srinath <gsrinath222@gmail.com>
2026-06-22 21:35:58 +0530

    Implement student calculation

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def calculate_grade(mark):
+
+    if mark >= 90:
+        return "A"
+
+    elif mark >= 80:
+        return "B"
+
+    elif mark >= 70:
+        return "C"
+
+    elif mark >= 60:
+        return "D"
+
+    else:
+        return "F"
